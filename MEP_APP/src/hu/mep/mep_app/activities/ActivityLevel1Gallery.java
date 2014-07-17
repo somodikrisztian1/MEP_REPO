a3876f4f0c5a9c5e9b6f74d452cc5cafe230be42
Bandi <bandijenei@gmail.com>
2014-07-17 15:40:37 +0200

    bula

--- MEP_APP/src/hu/mep/mep_app/activities/ActivityLevel1Gallery.java
+++ MEP_APP/src/hu/mep/mep_app/activities/ActivityLevel1Gallery.java
@@ -3,8 +3,6 @@ package hu.mep.mep_app.activities;
 import hu.mep.datamodells.Session;
 import hu.mep.mep_app.FragmentLevel1GalleryPicture;
 import hu.mep.mep_app.R;
-import hu.mep.utils.others.DepthPageTransformer;
-import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -13,7 +11,6 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.Window;
-import android.view.WindowManager;
 
 public class ActivityLevel1Gallery extends FragmentActivity {
 
@@ -34,7 +31,9 @@ public class ActivityLevel1Gallery extends FragmentActivity {
 		mPager = (ViewPager) findViewById(R.id.activity_firstlevel_gallery_viewpager);
 		mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
 		//mPager.setPageTransformer(true, new DepthPageTransformer());
+		
 		mPager.setAdapter(mPagerAdapter);
+		
 	}
 
 	
